3546f6cfe86a6cbe5489208e0cc2ad63a8134997
BlinkShine <wahyuajisulaiman@gmail.com>
2018-09-17 20:01:59 +0700

    fakebook

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Fakebook</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Selamat datang di Fakebook</h1>
+                        <p>Bergabunglah bersama jutaan orang lainnya...</p>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="login.php" class="btn btn-secondary">Masuk</a>
+                        <a href="register.php" class="btn btn-success">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img class="img img-responsive" src="img/connect.png" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,49 @@
+<?php require_once("src/login-source.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login Fakebook</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+
+        <p>&larr; <a href="index.php">Home</a>
+
+        <h4>Masuk ke Fakebook</h4>
+        <p>Belum punya akun? <a href="register.php">Daftar di sini</a></p>
+
+        <form action="" method="POST">
+
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input class="form-control" type="text" name="username" placeholder="Username atau email" />
+            </div>
+
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password" placeholder="Password" />
+            </div>
+
+            <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk" />
+
+        </form>
+            
+        </div>
+
+        <div class="col-md-6">
+            <!-- isi dengan sesuatu di sini -->
+        </div>
+
+    </div>
+</div>
+    
+</body>
+</html>
